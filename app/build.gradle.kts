6b8357d15c906c587ab5a1e331c70d559a944137
jcnpereira <casimiro@ipt.pt>
2024-05-23 21:03:49 +0100

    adição da biblioteca do Open Street Maps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,9 @@ android {
 
 dependencies {
 
+    // implementation ("org.osmdroid:osmdroid-android:6.1.10")
+    implementation (libs.osmdroid.android)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
